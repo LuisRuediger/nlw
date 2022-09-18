82dcfd887ed4b0a6566cf16c49a2e2b4acf5af88
Luis Ruediger <luisruediger27@gmail.com>
2022-09-18 00:27:22 -0300

    dasdsa

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -50,9 +50,7 @@ function App() {
 
       <Dialog.Root>
         <CreateAdBanner/>
-        <CreateAdModal>
-
-        </CreateAdModal>
+        <CreateAdModal/>
       </Dialog.Root>
     </div>
 
